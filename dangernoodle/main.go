39c7aff93c4d774f5027282db44865f5f5f19ae1
jsedano <jsedano@kenzan.com>
2019-09-11 22:26:01 -0500

    better way to move snake

--- dangernoodle/main.go
+++ dangernoodle/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image/color"
 	"log"
+	"time"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
@@ -28,7 +29,8 @@ const (
 )
 
 type game struct {
-	status         gameStatus
+	status gameStatus
+
 	snakeX, snakeY float64
 }
 
@@ -38,7 +40,9 @@ type snakepart struct {
 	d            direction
 }
 
-var snake = make([]*snakepart, 200, 1000)
+var snake = make([]*snakepart, 5, 1000)
+
+const snakesize = 4
 
 func init() {
 	var fx float64 = 100
@@ -47,7 +51,7 @@ func init() {
 	for i := range snake {
 		var s snakepart
 		s.i = imag
-		s.x = fx - 2*float64(i)
+		s.x = fx - snakesize*float64(i)
 		s.y = fy
 		s.d = right
 		snake[i] = &s
@@ -55,10 +59,14 @@ func init() {
 }
 
 func loadImage() (eimage *ebiten.Image) {
-	eimage, _ = ebiten.NewImage(2, 2, ebiten.FilterDefault)
+	eimage, _ = ebiten.NewImage(snakesize, snakesize, ebiten.FilterDefault)
 	eimage.Fill(color.White)
 	return
 }
+
+var lastMoved = time.Now()
+var lastInput direction = -1
+var speed = time.Millisecond * 100
 
 func update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
@@ -66,40 +74,43 @@ func update(screen *ebiten.Image) error {
 	}
 
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS()))
-
 	m := move()
-	if m != -1 {
-		if (m == right && snake[0].d != left) ||
-			(m == left && snake[0].d != right) ||
-			(m == up && snake[0].d != down) ||
-			(m == down && snake[0].d != up) {
-			snake[0].d = m
+	if time.Now().After(lastMoved.Add(speed)) {
+		lastMoved = time.Now()
+		if m != -1 {
+			if (m == right && snake[0].d != left) ||
+				(m == left && snake[0].d != right) ||
+				(m == up && snake[0].d != down) ||
+				(m == down && snake[0].d != up) {
+				snake[0].d = m
+			}
 		}
+
+		moveSnake()
 	}
-
-	moveSnake()
 	drawSnake(screen)
-
 	return nil
 }
 
 func moveSnake() {
-	for i := range snake {
-		switch snake[i].d {
-		case up:
-			snake[i].y = snake[i].y - 2
-		case down:
-			snake[i].y = snake[i].y + 2
-		case left:
-			snake[i].x = snake[i].x - 2
-		case right:
-			snake[i].x = snake[i].x + 2
-		}
+	n := snake[len(snake)-1]
+	n.d = snake[0].d
+	switch n.d {
+	case up:
+		n.x = snake[0].x
+		n.y = snake[0].y - snakesize
+	case down:
+		n.x = snake[0].x
+		n.y = snake[0].y + snakesize
+	case left:
+		n.x = snake[0].x - snakesize
+		n.y = snake[0].y
+	case right:
+		n.x = snake[0].x + snakesize
+		n.y = snake[0].y
 	}
 
-	for i := len(snake) - 1; i >= 1; i-- {
-		snake[i].d = snake[i-1].d
-	}
+	snake = append([]*snakepart{n}, snake[:len(snake)-1]...)
 
 }
 
@@ -117,18 +128,18 @@ func drawSnakePart(x float64, y float64, sp *ebiten.Image, screen *ebiten.Image)
 
 func move() direction {
 	if inpututil.IsKeyJustPressed(ebiten.KeyW) {
-		return up
+		lastInput = up
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyA) {
-		return left
+		lastInput = left
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyS) {
-		return down
+		lastInput = down
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyD) {
-		return right
+		lastInput = right
 	}
-	return -1
+	return lastInput
 }
 
 func main() {
